e655189d971ac48d11b953cc386e3aeeba8faa83
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2021-03-29 20:47:02 -0500

    Add more content. Fix styling issues.

--- public/sw.js
+++ public/sw.js
@@ -15,6 +15,6 @@ self.addEventListener('fetch', function(e) {
     e.respondWith(
         caches.match(e.request).then((response) => (
             response || fetch(e.request)
-        ))
+        ), console.log)
     ) 
 });
