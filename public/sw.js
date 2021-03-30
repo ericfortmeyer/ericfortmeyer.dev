cb3804abd0a2509f3b0b715db1e246706b326750
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2021-03-29 20:38:29 -0500

    Add more content. Fix styling issues.

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,7 @@
 const filesToCache = [
     './manifest.json',
-    './resources/css/style.min.css',
-    './resources/images/icon.png',
     './resources/images/icon-192.png',
     './resources/images/icon-512.png',
-    './resources/images/proautocleaners.webp',
-    './resources/images/proautocleaners.jp2',
-    './resources/images/pioneercleaning.webp',
-    './resources/images/pioneercleaning.jp2',
-    './resources/images/scheduler.webp',
-    './resources/images/scheduler.jp2',
     './resources/fonts/veteran-typewriter.ttf',
 ];
 const hostsRequiringCaching = ['ericfortmeyer.dev', 'iam.ericfortmeyer.dev', 'www.ericfortmeyer.dev', 'localhost'];
