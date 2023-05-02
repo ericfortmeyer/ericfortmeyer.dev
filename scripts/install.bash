4571af41a6823acf8cafe45cbe82255fc85fade4
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-05-01 21:16:49 -0500

    fix typo in install script

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 sudo rsync -vrtp ericfortmeyer.dev/public/ /var/www/html/
-suod mv server/nginx.conf /etc/nginx/sites-available/ericfortmeyer.conf
+sudo mv server/nginx.conf /etc/nginx/sites-available/ericfortmeyer.conf
 
