ad86758a519fa53afe188a373512bf967c13a28f
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-05-01 21:37:44 -0500

    install in home dir instead of /var/www

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
 
+HOSTNAME=ericfortmeyer.dev
+
 rsync \
   --progress \
   --verbose \
   --compress \
   --recursive \
   -e "ssh -o StrictHostKeyChecking=no" \
-  ~/Projects/ericfortmeyer/ericfortmeyer.dev/public/* root@baruq:/var/www/html/
+  ~/Projects/ericfortmeyer/$HOSTNAME/ root@baruq:$HOME/$HOSTNAME/
